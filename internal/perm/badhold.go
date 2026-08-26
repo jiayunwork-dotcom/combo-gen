a3b08db6a4d8958ec8b1680fb0ca9d45949d3a84
“jiayun” <jiayun_work@163.com>
2026-08-26 14:51:47 +0800

    fix: bindBadPerm 先 make permMemo，k 过大错误能返回调用方

--- internal/perm/badhold.go
+++ internal/perm/badhold.go
@@ -3,10 +3,21 @@ package perm
 var permMemo map[string]error
 
 func bindBadPerm(err error) error {
-	key := "perm"
-	if err != nil {
-		key = err.Error()
+	if permMemo == nil {
+		permMemo = make(map[string]error)
 	}
-	permMemo[key] = err
+	if err == nil {
+		return nil
+	}
+	key := err.Error()
+	if key == "" {
+		key = "perm"
+	}
+	copied := make(map[string]error, len(permMemo)+1)
+	for k, v := range permMemo {
+		copied[k] = v
+	}
+	copied[key] = err
+	permMemo = copied
 	return err
 }
