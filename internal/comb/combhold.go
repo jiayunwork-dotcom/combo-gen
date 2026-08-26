8bbf6553f270d21f54fd541034537b6f7dd56053
“jiayun” <jiayun_work@163.com>
2026-08-26 14:51:45 +0800

    fix: HoldCombLive 回本笔组合列表，不再被预置 [[z,z]] 盖掉

--- internal/comb/combhold.go
+++ internal/comb/combhold.go
@@ -1,11 +1,20 @@
 package comb
 
-var liveComb = [][]string{
-	{"z", "z"},
-}
+var liveComb [][]string
 
 func HoldCombLive(cur [][]string) [][]string {
-	out := liveComb
-	liveComb = cur
+	if cur == nil {
+		liveComb = nil
+		return nil
+	}
+	out := make([][]string, len(cur))
+	for i := range cur {
+		out[i] = append([]string(nil), cur[i]...)
+	}
+	saved := make([][]string, len(out))
+	for i := range out {
+		saved[i] = append([]string(nil), out[i]...)
+	}
+	liveComb = saved
 	return out
 }
